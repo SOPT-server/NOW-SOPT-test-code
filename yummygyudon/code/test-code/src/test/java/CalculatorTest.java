cc9e26407a2be3db5015ebbfc6759260f95e8fe5
yummygyudon <bang2brew@gmail.com>
2024-04-16 03:04:34 +0900

    TEST : 선 테스트 코드 작성

---
+++ yummygyudon/code/test-code/src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void plus() {
+        // `Calculator`라는 클래스 구현 없이 Test 코드 선 작성
+        int result = Calculator.plus(1, 2);
+        Assertions.assertEquals(3, result);
+    }
+}
